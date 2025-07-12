4d687b21e731a6a45c25cebefcc770c436142bf9
Plutopenguin11 <124006402+PlutoPenguin11@users.noreply.github.com>
2025-07-11 21:42:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,18 @@
-const GeminiKey = process.env.GeminiKey;
 //const ElevenlabsKey = process.env.ElevenlabsKey;
 const ElevenlabsKey = process.env.ElevenlabsTestingKey;
 
 import { GoogleGenAI } from "@google/genai";
 
-const chat_model = "chatgpt";
 
-if (chat_model == "chatgpt") {
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const ai = new GoogleGenAI({});
 
-    const openai = new OpenAI();
-
-    const response = await openai.responses.create({
-        model: "gpt-4o",
-        input: "Tell me a three sentence bedtime story about a unicorn."
-    });
-
-    console.log(response);
-
-} else {
-
-    // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-    const ai = new GoogleGenAI({});
-
-    async function main() {
-    const response = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
-        contents: "Explain how AI works in a few words",
-    });
-    console.log(response.text);
-    }
-
-    main();
+async function main() {
+const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+});
+console.log(response.text);
 }
+
+main();

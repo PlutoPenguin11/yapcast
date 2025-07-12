a3529fa151a4baec2f4b0c4faa91c7fc7f8f92e8
Plutopenguin11 <124006402+PlutoPenguin11@users.noreply.github.com>
2025-07-11 20:03:01 -0400

    Installed gemini dependencies

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-const GeminiKey = process.env.GeminiKey;
+/*
+
+const GeminiKey = process.env.GEMINI_API_KEY;
+console.log(GeminiKey)
+
+
+
+
+*/
+
 //const ElevenlabsKey = process.env.ElevenlabsKey;
 const ElevenlabsKey = process.env.ElevenlabsTestingKey;
 
-console.log(GeminiKey)
+
+import { GoogleGenAI } from "@google/genai";
+
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const ai = new GoogleGenAI({});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Please give a brief affirmative response confirming you recieved this prompt",
+  });
+  console.log(response.text);
+}
+
+main();

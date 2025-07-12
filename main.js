3e6ea3b0415cc2349d978c444ea695a6a8f57b1e
Plutopenguin11 <124006402+PlutoPenguin11@users.noreply.github.com>
2025-07-11 21:39:47 -0400

    Gemini api requrements

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-/*
-
-const GeminiKey = process.env.GEMINI_API_KEY;
-console.log(GeminiKey)
-
-
-
-
-*/
-
+const GeminiKey = process.env.GeminiKey;
 //const ElevenlabsKey = process.env.ElevenlabsKey;
 const ElevenlabsKey = process.env.ElevenlabsTestingKey;
 
-
 import { GoogleGenAI } from "@google/genai";
 
-// The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({});
+const chat_model = "chatgpt";
 
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Please give a brief affirmative response confirming you recieved this prompt",
-  });
-  console.log(response.text);
+if (chat_model == "chatgpt") {
+
+    const openai = new OpenAI();
+
+    const response = await openai.responses.create({
+        model: "gpt-4o",
+        input: "Tell me a three sentence bedtime story about a unicorn."
+    });
+
+    console.log(response);
+
+} else {
+
+    // The client gets the API key from the environment variable `GEMINI_API_KEY`.
+    const ai = new GoogleGenAI({});
+
+    async function main() {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: "Explain how AI works in a few words",
+    });
+    console.log(response.text);
+    }
+
+    main();
 }
-
-main();

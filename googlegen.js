a4bbaac6bfe22264c20609941d4f70addf1bc663
Plutopenguin11 <124006402+PlutoPenguin11@users.noreply.github.com>
2025-07-28 23:03:59 -0400

    Update googlegen.js

--- googlegen.js
+++ googlegen.js
@@ -1,9 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 import 'dotenv/config';
 const ai = new GoogleGenAI({});
-
-import wav from 'wav';
-
 import { Buffer } from 'buffer';
 import Speaker from 'speaker';
 
@@ -17,52 +14,37 @@ export async function generateText(prompt) {
   return response.text;
 }
 
-export async function generateVoice(text, voiceName) {
-      const ai = new GoogleGenAI({});
+export async function generateVoice(text, name) {
+   const ai = new GoogleGenAI({});
 
-      let message = `Say in a casual, natural tone like you're in a chill podcast:
-      ${text}`
+   const message = `Say in a casual, natural tone like you're in a chill podcast:
+   ${text}`;
 
-      const response = await ai.models.generateContent({
-         model: "gemini-2.5-flash-preview-tts",
-         contents: [{ parts: [{ text: message }] }],
-         config: {
-               responseModalities: ['AUDIO'],
-               speechConfig: {
-                  voiceConfig: {
-                     prebuiltVoiceConfig: { voiceName: voiceName },
-                  },
-               },
+   const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash-preview-tts",
+      contents: [{ parts: [{ text: message }] }],
+      config: {
+         responseModalities: ['AUDIO'],
+         speechConfig: {
+            voiceConfig: {
+               prebuiltVoiceConfig: { voiceName: name },
+            },
          },
-      });
+      },
+   });
 
-   async function saveWaveFile(
-      filename,
-      pcmData,
-      channels = 1,
-      rate = 24000,
-      sampleWidth = 2,
-   ) {
-      return new Promise((resolve, reject) => {
-         const writer = new wav.FileWriter(filename, {
-               channels,
-               sampleRate: rate,
-               bitDepth: sampleWidth * 8,
-         });
+   const data = response.candidates?.[0]?.content?.parts?.[0]?.inlineData?.data;
+   const audioBuffer = Buffer.from(data, 'base64');
 
-         writer.on('finish', resolve);
-         writer.on('error', reject);
+   const speaker = new Speaker({
+      channels: 1,
+      bitDepth: 16,
+      sampleRate: 24000,
+      signed: true,
+   });
 
-         writer.write(pcmData);
-         writer.end();
-      });
-   }
-
-      const data = response.candidates?.[0]?.content?.parts?.[0]?.inlineData?.data;
-      const audioBuffer = Buffer.from(data, 'base64');
-
-      const fileName = 'out.wav';
-      await saveWaveFile(fileName, audioBuffer);
-};
+   speaker.write(audioBuffer);
+   speaker.end();
+}
 
 console.log("Promise has been created and handlers attached. Waiting for settlement...");
